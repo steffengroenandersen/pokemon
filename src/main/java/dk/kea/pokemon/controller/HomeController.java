57ebe0af99f39cba15022cc6b780a61995f1756b
steffengroenandersen <steffengroenandersen@gmail.com>
2023-03-17 19:10:27 +0100

    Added index and application.properties

--- src/main/java/dk/kea/pokemon/controller/HomeController.java
+++ src/main/java/dk/kea/pokemon/controller/HomeController.java
@@ -1,9 +1,22 @@
 package dk.kea.pokemon.controller;
 
+import dk.kea.pokemon.repository.PokemonRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    PokemonRepository pokemonRepository;
 
+    public Homecontroller(PokemonRepository pokemonRepository){
+        this.pokemonRepository = pokemonRepository;
+    }
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("pokemons", pokemonRepository.getAllPokemons());
+        return "index";
+    }
 }
